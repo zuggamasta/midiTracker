11997e7aee1e47592e6e9d5a500a8491173681f9
zuggamasta <34277191+zuggamasta@users.noreply.github.com>
2023-04-23 10:53:59 +0200

    plays one C3

--- hello.py
+++ hello.py
@@ -5,6 +5,16 @@ import time
 
 import rtmidi
 
+midiout = rtmidi.MidiOut()
+available_ports = midiout.get_ports()
+
+if available_ports:
+    midiout.open_port(0)
+else:
+    midiout.open_virtual_port("My virtual output")
+
+
+
 def main(stdscr):
     bpm = 120
 
@@ -45,7 +55,15 @@ def main(stdscr):
                             stdscr.addstr(y+offset_y, x*3+offset_x, data[y][x], curses.A_REVERSE)
 
             stdscr.refresh()
-            
+
+            with midiout:
+                note_on = [0x90, 60, 112] # channel 1, middle C, velocity 112
+                note_off = [0x80, 60, 0]
+                midiout.send_message(note_on)
+                time.sleep(0.3)
+                midiout.send_message(note_off)
+                time.sleep(0.1)
+
             time.sleep(60/bpm/8)
 
     stdscr.getch()
