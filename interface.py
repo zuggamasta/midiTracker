0410dc22b26c399b5792195b4cef93026030829d
zuggamasta <34277191+zuggamasta@users.noreply.github.com>
2023-05-01 21:13:43 +0200

    first data pass, added digagram and documentation folder

--- interface.py
+++ interface.py
@@ -15,10 +15,11 @@ SCENES = ["song","chain","phrase","config"]
 NOTES_LOOKUP = ['C ','C#','D ','Eb','E ','F ','F# ','G ','G#','A ','Bb','B ' ]
 SLOT_WIDTH = 4
 
+MAX_CHANNELS = 6
+
 
 pos = [0,0]
 
-current_chain = 0
 
 is_song_playing = True
 
@@ -26,68 +27,120 @@ bpm = 120
 
 # scene infos
 current_scene = 0
+active_data = []
 
 # Generates empty CHAIN slots for Song 
 current_song = 0
-song_data = [[None for _ in range(16)] for _ in range(6)]
+song0 = [[None for _ in range(16)] for _ in range(6)]
 
-song_data[0][0] = 0x00
+song0[0][0] = 0x00
 
+song_data = []
+song_data.append(song0)
 
+current_chain = 0
 chain_data = []
-chain0 =  [0x00,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None]     # PHRASES
+chain0 =    [[0x01,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None],  # PHRASES
+             [0x10,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None]]   # TRANSPOSE
 chain_data.append(chain0)
 
+current_phrase = 0
 phrase_data = []
-phrase0 =  [0x5c,None,None,None,None,None,None,None, # NOTES
-            0x5c,None,None,None,None,None,None,None]     
+phrase0 =  [[0x5c,None,None,None,None,None,None,None, # NOTES
+             0x5c,None,None,None,None,None,None,None],
+            [0x5c,None,None,None,None,None,None,None, # CMD
+             0x5c,None,None,None,None,None,None,None]]
+
+phrase1 =  [[0x5c,None,None,None,None,None,None,None, # NOTES
+             0x5c,0xff,0xff,0xff,0xff,0xff,None,None],
+            [0x5c,None,None,None,None,None,None,None, # CMD
+             0x5c,None,None,None,None,None,None,None]]
+  
 phrase_data.append(phrase0)
 
+phrase_data.append(phrase1)
+
+phrase2 =  [[0x5c,None,None,None,None,None,None,None, # NOTES
+             0x5c,0xff,0xff,0xff,0xff,0xff,None,None],
+            [0x5c,None,None,None,None,None,None,None, # CMD
+             0x5c,None,None,None,None,None,None,None]]
+
+phrase_data.append(phrase2)
+
+
+
+
 config_data = []
-config=  [0x00,None,0xff,0xab]     
+config=  [[0x00,None,0xff,0xab]   ]  
 config_data.append(config)
 
 def updateInput(scr,data,max_column,max_row):
+
+    global pos
+    global current_scene
+    global current_song
+    global current_chain
+    global current_phrase
+    global active_data
+
+    if current_scene == 0:
+        active_data = current_song
+    elif current_scene == 1:
+        active_data = current_chain
+    elif current_scene == 2:
+        active_data = current_phrase
+    elif current_scene == 3:
+        active_data == 0
+    
+
     try:
         key = scr.getkey()
     except:
         key = None
 
-    global pos
-    global current_scene
+
 
     # SWITCH SCENE 
     if key == "kRIT5":
         current_scene += 1
     elif key == "kLFT5":
         current_scene -= 1
+
+     # SWITCH CHAIN / SONG / PHRASE  kUP5 kDN5
+    elif key == "kUP5":
+        current_phrase += 1
+    elif key == "kDN5":
+        current_phrase -= 1
  
+    elif key == " ":
+        play_chain(0,0,scr)
+
     # MODIFY DATA
     elif key == "KEY_SR":
-        if data[pos[0]][pos[1]] == None:
-            data[pos[0]][pos[1]] = 0x0
+        if data[active_data][pos[0]][pos[1]] == None:
+            data[active_data][pos[0]][pos[1]] = 0x0
         else:
-            data[pos[0]][pos[1]] += 0x1
+            data[active_data][pos[0]][pos[1]] += 0x1
     elif key == "KEY_SF":
-        if data[pos[0]][pos[1]] == None:
-            data[pos[0]][pos[1]] = 0x0
+        if data[active_data][pos[0]][pos[1]] == None:
+            data[active_data][pos[0]][pos[1]] = 0x0
         else:
-            data[pos[0]][pos[1]] -= 0x1
+            data[active_data][pos[0]][pos[1]] -= 0x1
     elif key == "KEY_SRIGHT":
-        if data[pos[0]][pos[1]] == None:
-            data[pos[0]][pos[1]] = 0x0
+        if data[active_data][pos[0]][pos[1]] == None:
+            data[active_data][pos[0]][pos[1]] = 0x0
         else:
-            data[pos[0]][pos[1]] += 12
+            data[active_data][pos[0]][pos[1]] += 12
     elif key == "KEY_SLEFT":
-        if data[pos[0]][pos[1]] == None:
-            data[pos[0]][pos[1]] = 0x0
+        if data[active_data][pos[0]][pos[1]] == None:
+            data[active_data][pos[0]][pos[1]] = 0x0
         else:
-            data[pos[0]][pos[1]] -= 12
+            data[active_data][pos[0]][pos[1]] -= 12
     elif key == "x":
-        if data[pos[0]][pos[1]] == None:
+        if data[active_data][pos[0]][pos[1]] == None:
             pass
         else:
-            data[pos[0]][pos[1]] = None
+            data[active_data][pos[0]][pos[1]] = None
 
     # MOVE CURSOR
     elif key == "KEY_UP":
@@ -122,11 +175,11 @@ def updateInput(scr,data,max_column,max_row):
     if pos[1] >= max_row:
         pos[1] = 0
 
-    if data[pos[0]][pos[1]] != None:
-        if data[pos[0]][pos[1]] < 0:
-            data[pos[0]][pos[1]] = 255
-        if data[pos[0]][pos[1]] > 255:
-            data[pos[0]][pos[1]] = 0
+    if data[active_data][pos[0]][pos[1]] != None:
+        if data[active_data][pos[0]][pos[1]] < 0:
+            data[active_data][pos[0]][pos[1]] = 255
+        if data[active_data][pos[0]][pos[1]] > 255:
+            data[active_data][pos[0]][pos[1]] = 0
 
     scr.refresh()
 
@@ -137,7 +190,7 @@ def drawData(scr,data,max_column,max_row,is_note):
 
     for column in range(max_row):
         for row in range(max_column):
-            slot = data[row][column]
+            slot = data[active_data][row][column]
             note = 0x0
             render_slot = ""
             if slot == None:
@@ -157,13 +210,36 @@ def drawData(scr,data,max_column,max_row,is_note):
     scr.refresh()
     data_win.refresh()
 
-def note_on(data,max_column,max_row):
 
+song_step = 0
+
+def play_song():
+    global song_step
+    for channel in range(MAX_CHANNELS):
+        active_chain_no = song_data[current_song][channel][song_step]
+        play_chain(active_chain_no,channel)
+        pass
+    song_step += 1 
+    pass 
+
+# CHAIN_DATA[chain_number][0=phrase,1=transpose][chain_step]
+def play_chain(chain_no,channel,scr):
+    chain_step = 0
+      
+    if chain_data[chain_no][0][chain_step] != None:
+        scr.addstr(3,36,f"debug: {chain_data[chain_no][0][chain_step]:02x}", curses.A_ITALIC)
+        
+        pass
+    pass
+
+def note_on(data,active_data,max_column,max_row):
+    global last_notes
+    last_notes = [None,None,None,None,None,None]
+    
     for column in range(max_row):
         for row in range(max_column):
-            slot = data[row][column]
-            note = 0x0
-            render_slot = ""
+            slot = data[active_data][row][column]
+            
             if slot == None:
                 # not a musical note
                 pass
@@ -219,7 +295,7 @@ def main(stdscr):
             case 0:
                 # SONG VIEW
                 # Header
-                stdscr.addstr(1,2,f"{SCENES[current_scene]} 00      ")
+                stdscr.addstr(1,2,f"{SCENES[current_scene]} {current_song:02}      ")
                 stdscr.addstr(2,2,f"Trk1Trk2Trk3Trk4Trk5Trk6",curses.A_REVERSE)
                 
                 # DATA
@@ -231,12 +307,12 @@ def main(stdscr):
             case 1:
                 # CHAIN VIEW
                 # Header
-                stdscr.addstr(1,2,f"{SCENES[current_scene]} 00      ")
+                stdscr.addstr(1,2,f"{SCENES[current_scene]} {current_chain:02}      ")
                 stdscr.addstr(2,2,f"PhrsTrsp",curses.A_REVERSE)
                 stdscr.addstr("                          ")
 
                 # DATA
-                channels = 1
+                channels = 2
                 steps = 16
                 updateInput(stdscr,chain_data,channels,steps)
                 drawColumNumbers(stdscr)
@@ -244,12 +320,12 @@ def main(stdscr):
             case 2:
                 # PHRASE VIEW
                 # Header
-                stdscr.addstr(1,2,f"{SCENES[current_scene]} 00      ")
+                stdscr.addstr(1,2,f"{SCENES[current_scene]} {current_phrase:02}      ")
                 stdscr.addstr(2,2,f"Note CMD",curses.A_REVERSE)
                 stdscr.addstr("                          ")
 
                 # DATA
-                channels = 1
+                channels = 2
                 steps = 16
                 updateInput(stdscr,phrase_data,channels,steps)
                 drawColumNumbers(stdscr)
@@ -270,7 +346,7 @@ def main(stdscr):
                 channels = 1
                 steps = 4
                 updateInput(stdscr,config_data,channels,steps)
-                drawData(stdscr,song_data,channels,steps,is_note=False)
+                drawData(stdscr,config_data,channels,steps,is_note=False)
                 pass
 
             case _:
